9fddff2bb11ee0c05b9ce8d1ce32f7ee574a1f7c
csbs269036-star <csbs269036@saranathan.ac.in>
2025-11-06 13:48:46 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,17 +1,21 @@
-
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
-num=int(input("num:"))
 def greet():
-    """Return a custom friendly HTTP greeting."""
-    print("Hi Everyone ! I am running through cloud shell ")
-    if (num>0 & num<100):
-        num=num+273
-        print(f"Celius to Faherant: {num}")
+    """Return a custom friendly HTTP greeting with temperature conversion."""
+    num = request.args.get("num", type=int)  # get ?num= value from URL
+    message = "Hi Everyone! I am running through cloud shell. "
+
+    if num is not None and num > 0 and num < 100:
+        kelvin = num + 273
+        fahrenheit = (num * 9/5) + 32
+        message += f"Celsius: {num}, Kelvin: {kelvin}, Fahrenheit: {fahrenheit}"
+    else:
+        message += "Please provide a Celsius value between 0 and 100 using ?num= in the URL."
+
+    return message
 
 if __name__ == "__main__":
     app_host = "http://127.0.0.1:8080"
