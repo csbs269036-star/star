9876e00482babecc2c58901f8859bb7c841bee4a
csbs269036-star <csbs269036@saranathan.ac.in>
2025-11-06 13:31:57 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def greet():
     """Return a custom friendly HTTP greeting."""
-    #return "Hi Everyone ! I am running through cloud shell"
-    num=int(input("num"))
+    return "Hi Everyone ! I am running through cloud shell"
     if (num>0 & num<100):
         num=num+273
         print(f"Celius to Faherant: {num}")
@@ -16,4 +15,5 @@ def greet():
 if __name__ == "__main__":
     app_host = "http://127.0.0.1:8080"
     print(f"App host link: {app_host}")
+    num=int(input("num"))
     app.run(host="127.0.0.1", port=8080, debug=True)
